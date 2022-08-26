2da903df0d738271d483b7cd238f97686488f0c6
hello <hello>
2022-08-27 00:08:09 +0900

    수업복습

--- LEDBoard2/ViewController.swift
+++ LEDBoard2/ViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        print(10)
-        print(20)
+
     }
 
 
